c60b84f4bb1f93c29952b42abf7735d3b264eb69
Владимир Тетерин <bigsinner@yandex.ru>
2022-03-12 06:36:11 +1000

    Добавьте файлы проекта.

---
+++ Less17Task3.cpp
@@ -0,0 +1,32 @@
+﻿/*
+Написать функцию, которая принимаем указатель на char, по которому лежит строка.
+Функция должна возвращать true, если вторая строка является подстрокой первой.
+*/
+
+#include <iostream>
+using namespace std;
+
+bool substr(char a[], char b[])
+{
+    
+    for (int i = 0; *(a + i) != '\0'; i++)
+    {
+        bool bCorrect = true;
+        for (int n = 0; *(b + n) != '\0'; n++)
+        {
+            if (*(b + n) != *(a + i + n)) bCorrect = false;
+        }
+        if (bCorrect) return true;
+    }
+    return false;
+}
+
+int main()
+{
+    char a[] = "Hello world";
+    char b[] = "wor";
+    char c[] = "bababa";
+
+    cout << boolalpha << substr(a, b) << " " << substr(a, c);
+
+}
